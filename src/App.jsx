f298a4be022a4d01888e5b5276897222b593ba77
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-12-10 19:36:55 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,7 @@ import UpdatePage from "./pages/UpdatePage";
 
 function App() {
     const [showLoader, setShowLoader] = useState(true); // default value of the loader is true (loader displayed)
-    const [isAuth, setIsAuth] = useState(
-        localStorage.getItem("isAuth")
-    ); // default value comes from localStorage
+    const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth")); // default value comes from localStorage
 
     useEffect(() => {
         const auth = getAuth();
@@ -32,59 +30,47 @@ function App() {
         });
     }, []);
 
-    // variable holding all private routes including the nav bar
-    const privateRoutes = (
-        <>
-            <Nav />
+    return (
+        <main>
+            {showLoader && <Loader />} {/* if showLoader is true, display the loader */}
+            {isAuth && <Nav />} {/* if isAuth is true, display the nav */}
             <Routes>
                 <Route
                     path="/"
-                    element={<PostsPage showLoader={setShowLoader} />}
+                    // if isAuth is true, display the PostsPage, else redirect to sign-in
+                    element={isAuth ? <PostsPage showLoader={setShowLoader} /> : <Navigate to="/sign-in" />}
                 />
                 <Route
                     path="/create"
-                    element={
-                        <CreatePage showLoader={setShowLoader} />
-                    }
+                    // if isAuth is true, display the CreatePage, else redirect to sign-in
+                    element={isAuth ? <CreatePage showLoader={setShowLoader} /> : <Navigate to="/sign-in" />}
                 />
                 <Route
                     path="/posts/:id"
-                    element={
-                        <UpdatePage showLoader={setShowLoader} />
-                    }
+                    // if isAuth is true, display the UpdatePage, else redirect to sign-in
+                    element={isAuth ? <UpdatePage showLoader={setShowLoader} /> : <Navigate to="/sign-in" />}
                 />
                 <Route
                     path="/profile"
-                    element={
-                        <ProfilePage showLoader={setShowLoader} />
-                    }
+                    // if isAuth is true, display the ProfilePage, else redirect to sign-in
+                    element={isAuth ? <ProfilePage showLoader={setShowLoader} /> : <Navigate to="/sign-in" />}
                 />
-                <Route path="*" element={<Navigate to="/" />} />
+                <Route
+                    path="/sign-in"
+                    // if isAuth is true, redirect to home, else display the SignInPage
+                    element={isAuth ? <Navigate to="/" /> : <SignInPage showLoader={setShowLoader} />}
+                />
+                <Route
+                    path="/sign-up"
+                    // if isAuth is true, redirect to home, else display the SignUpPage
+                    element={isAuth ? <Navigate to="/" /> : <SignUpPage showLoader={setShowLoader} />}
+                />
+                <Route
+                    path="*"
+                    // if no route matches, redirect to home
+                    element={<Navigate to="/" />}
+                />
             </Routes>
-        </>
-    );
-
-    // variable holding all public routes without nav bar
-    const publicRoutes = (
-        <Routes>
-            <Route
-                path="/sign-in"
-                element={<SignInPage showLoader={setShowLoader} />}
-            />
-            <Route
-                path="/sign-up"
-                element={<SignUpPage showLoader={setShowLoader} />}
-            />
-            <Route path="*" element={<Navigate to="/sign-in" />} />
-        </Routes>
-    );
-
-    // if user is authenticated, show privateRoutes, else show publicRoutes
-    // also, display or display not the <Loader/> based on showLoader state
-    return (
-        <main>
-            {isAuth ? privateRoutes : publicRoutes}
-            {showLoader && <Loader />}
         </main>
     );
 }
